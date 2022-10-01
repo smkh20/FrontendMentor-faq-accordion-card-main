4a3248238b566c6bb79aa89b20993c4a31c74958
smkh <mmm.smkh.mmm@gmail.com>
2022-10-01 15:35:23 +0330

    Woman component creat

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Question from "./Question";
+import {ReactComponent as Woman} from "./Illustration-woman-online-mobile.svg";
 function App() {
   return (
     <div className="main">
-      <Question />
+      <Woman></Woman>
+      {/* <Question /> */}
     </div>
   );
 }
