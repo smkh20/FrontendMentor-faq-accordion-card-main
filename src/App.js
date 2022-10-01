230e6189e5d0e7b6e13d6ed4a000534e1291050e
smkh <mmm.smkh.mmm@gmail.com>
2022-10-01 16:29:23 +0330

    try add pic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import Question from "./Question";
-import {ReactComponent as Woman} from "./Illustration-woman-online-mobile.svg";
 function App() {
   return (
     <div className="main">
-      <Woman></Woman>
-      {/* <Question /> */}
     </div>
   );
 }
