97412b3f46cb6199595cfcbff32419764ab13701
smkh <mmm.smkh.mmm@gmail.com>
2022-10-03 14:26:13 +0330

    arr.map in Container component add

--- src/Container.js
+++ src/Container.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import WomanPic from "./WomanPic";
 import Question from "./Question";
-
+import arr from "./data.js";
 export const Container = () => {
+  const [showItemIndex, setIndex] = useState(null);
+
+  const toggleShow = (index) => {
+    if (showItemIndex === index) {
+      setIndex(null);
+    } else {
+      setIndex(index);
+    }
+  };
   return (
     <div className="main">
       <WomanPic />
-      <h1 style={{ padding: "20px" }}>FAQ</h1>
-      <Question />
+      <h1 style={{ padding: "20px", color: "var(--txtDesaturatedBlue)" }}>FAQ</h1>
+      <div className="questioncontainer">
+        {arr.map((faq, index) => (
+          <Question key={index} faq={faq} toggle={() => toggleShow(index)} show={showItemIndex === index} />
+        ))}
+      </div>
     </div>
   );
 };
