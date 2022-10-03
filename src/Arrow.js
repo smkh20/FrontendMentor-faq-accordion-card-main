9b86cc0d9e87577ca63e539665496f2c059c3018
smkh <mmm.smkh.mmm@gmail.com>
2022-10-03 11:01:12 +0330

    creat Arrow component

---
+++ src/Arrow.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Arrow = () => {
+  return (
+    <svg width="10" height="7" xmlns="http://www.w3.org/2000/svg">
+      <path d="M1 .799l4 4 4-4" stroke="#F47B56" strokeWidth="2" fill="none" fillRule="evenodd" />
+    </svg>
+  );
+};
+
+export default Arrow;
